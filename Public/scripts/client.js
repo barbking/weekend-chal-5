97b44aa05d64c3867aba54bdf92ba9836c061f16
Barb King <barbgross@aol.com>
2017-05-12 22:44:46 -0500

    initial file set up

---
+++ Public/scripts/client.js
@@ -0,0 +1,31 @@
+//requires
+//node modules
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require( 'body-parser' );
+var mongoose = require( 'mongoose' );
+// 27017 is default mongo port
+mongoose.connect( 'localhost:27017/movies' );
+//schema
+var ourSchema = mongoose.Schema({
+  tile: String,
+  description: String
+});
+//model
+var favorites = mongoose.model( 'favorites', ourSchema );
+//uses
+app.use(express.static('public'));
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+// This should be the last route
+// /* is wildcard will respond to all requests
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+// globals
+var port = process.env.PORT || 3456;
+// spin up server
+app.listen( port, function() {
+  console.log( 'server up on:', port );
+});
